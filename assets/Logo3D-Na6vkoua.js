5d2d1bccc280c2f3d485448bcb797feee5fde21b
arnautxu <arnautxu@users.noreply.github.com>
2026-04-28 09:21:26 +0000

    deploy: dde08d4ba7b3166e3d5cc26e885e7aea594ccc80

--- assets/Logo3D-Na6vkoua.js
+++ assets/Logo3D-Na6vkoua.js
@@ -1,4 +1,4 @@
-import{r as st,a as hS,g as fS,j as un,p as dS}from"./index-Dfepdd3g.js";/**
+import{r as st,a as hS,g as fS,j as un,p as dS}from"./index-CY5tNbhU.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
